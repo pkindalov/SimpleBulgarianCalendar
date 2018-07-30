e3413d768491d266ba6646d8ab0b18c3b8e415e2
pkindalov <panayotkindalov@gmail.com>
2018-07-30 16:13:21 +0300

    Little refactoring on prevNextButtons.js file. Created separately methods to handle changing month. Added keydown event , so months can be changed only by left and right keys on keybord now.

--- js/prevNextButtons.js
+++ js/prevNextButtons.js
@@ -1,5 +1,26 @@
 const END_OF_MONTHS = 12
 
+function showPrevMonth(start, value){
+	start = $('.selectMonth option:selected').val()
+	start--
+	if(start < 1) start = END_OF_MONTHS
+	value = '' + start
+		
+	$('.selectMonth').val(value).trigger('change')
+}
+
+
+function showNextMonth(start, value){
+	start = $('.selectMonth option:selected').val()
+	start++
+	if(start > END_OF_MONTHS) start = 1
+	value = '' + start	
+	$('.selectMonth').val(value).trigger('change')
+}
+
+
+
+
 
 $(document).ready(function(){
 		let start = Number($('.selectMonth').val())
@@ -9,24 +30,37 @@ $(document).ready(function(){
 
 	$('a[name="prevMonth"]').click(function(event){
 		event.preventDefault()
-		start = $('.selectMonth option:selected').val()
-		start--
-		if(start < 1) start = END_OF_MONTHS
-		value = '' + start
+
+		start = showPrevMonth(start, value)
+		// start = $('.selectMonth option:selected').val()
+		// start--
+		// if(start < 1) start = END_OF_MONTHS
+		// value = '' + start
 		
-		$('.selectMonth').val(value).trigger('change')	
+		// $('.selectMonth').val(value).trigger('change')
+
 
 	}) 
 
 
 	$('a[name="nextMonth"]').click(function(event){
 		event.preventDefault()
-		start = $('.selectMonth option:selected').val()
-		start++
-		if(start > END_OF_MONTHS) start = 1
-
-		value = '' + start	
-		$('.selectMonth').val(value).trigger('change')
+		start = showNextMonth(start, value)
 	})
+
+
+
+	$(document).keydown(function(event){
+		// alert(event.which)
+		switch(event.which){
+			case 37:
+				start = showPrevMonth(start, value)
+				break
+			case 39:
+				start = showNextMonth(start, value)
+				break		
+		}
+	})
+
 
 })
